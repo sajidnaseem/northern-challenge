5ea001ee0182cca474fc9733bb4265195a6c2dbc
sajidnaseem <sajid_akhunzada@yahoo.com>
2020-06-20 14:11:06 -0600

    favicon issue

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/challenge'));
+app.use(express.static(__dirname + '/dist/challenge'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile('index.html',{root: '/dist/challenge/'});
+  res.sendFile(path.join(__dirname+'/dist/challenge/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
 
